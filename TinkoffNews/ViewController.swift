4876a9002325f0cfedf019a7abd63a7f5d6d06a3
Daniil <msbinarycat@gmail.com>
2020-01-30 13:14:31 +0400

    Initial Commit

---
+++ TinkoffNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinkoffNews
+//
+//  Created by Daniil on 30.01.2020.
+//  Copyright © 2020 Daniil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
